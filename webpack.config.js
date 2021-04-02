d4163c16077bf61ee2ea8c42b121d81b3876846e
Martinus Hadiwinata <martinus.ardian@gmail.com>
2021-04-02 00:54:34 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+
+const path = require( 'path' );
+module.exports = {
+   context: __dirname,
+   entry: './src/index.js',
+   module: {
+      rules: [
+        { test: /\.hbs$/, loader: "handlebars-loader" }
+      ]
+    },
+   output: {
+      path: path.resolve( __dirname, 'dist' ),
+      filename: 'main.js',
+   },
+   devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+   },
+   plugins: [
+      new HtmlWebPackPlugin({
+          title: 'App Boilerplate',
+          template: 'index.hbs'
+      })
+   ]
+};
