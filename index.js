94398cbbd173cd9e91117a1079016490a9716358
Martinus Hadiwinata <martinus.ardian@gmail.com>
2021-04-05 22:32:33 -0400

    Move index.js one level up, add proper js code file

---
+++ index.js
@@ -0,0 +1,5 @@
+// Javascripts
+import('./src/app.js');
+
+// Styles
+import('./styles/app.css');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
 const path = require( 'path' );
 module.exports = {
    context: __dirname,
-   entry: './src/index.js',
+   entry: './index.js',
    module: {
       rules: [
         { test: /\.hbs$/, loader: "handlebars-loader" },
